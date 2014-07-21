cb8ed039675847b676c05df7283a690b7fda863d
Jean-Baptiste Mouret <mouret@isir.upmc.fr>
2014-07-22 00:46:05 +0200

    cleaning up

--- src/limbo/rprop.hpp
+++ src/limbo/rprop.hpp
@@ -3,22 +3,29 @@
 
 #include <Eigen/Core>
 #include <boost/math/special_functions/sign.hpp>
+
+
 namespace rprop {
 
+  // partly inspired by libgp: https://github.com/mblum/libgp
+  // reference :
+  // Blum, M., & Riedmiller, M. (2013). Optimization of Gaussian
+  // Process Hyperparameters using Rprop. In European Symposium
+  // on Artificial Neural Networks, Computational Intelligence
+  // and Machine Learning.
   template<typename F1, typename F2>
   Eigen::VectorXd optimize(const F1& func, const F2& grad_func, int param_dim, int n) {
     // params
-    double Delta0 = 0.1;
-    double Deltamin = 1e-6;
-    double Deltamax = 50;
+    double delta0 = 0.1;
+    double deltamin = 1e-6;
+    double deltamax = 50;
     double etaminus = 0.5;
     double etaplus = 1.2;
     double eps_stop = 0.0;
 
-    Eigen::VectorXd Delta = Eigen::VectorXd::Ones(param_dim) * Delta0;
+    Eigen::VectorXd delta = Eigen::VectorXd::Ones(param_dim) * delta0;
     Eigen::VectorXd grad_old = Eigen::VectorXd::Zero(param_dim);
-    Eigen::VectorXd params = (Eigen::VectorXd::Random(param_dim).array() + 1); //0.5
-    //  Eigen::VectorXd::Constant(param_dim, 1);
+    Eigen::VectorXd params = (Eigen::VectorXd::Random(param_dim).array() + 1);
     Eigen::VectorXd best_params = params;
     double best = log(0);
 
@@ -28,9 +35,9 @@ namespace rprop {
       grad_old = grad_old.cwiseProduct(grad);
       for (int j = 0; j < grad_old.size(); ++j) {
         if (grad_old(j) > 0) {
-          Delta(j) = std::min(Delta(j) * etaplus, Deltamax);
+          delta(j) = std::min(Delta(j) * etaplus, deltamax);
         } else if (grad_old(j) < 0) {
-          Delta(j) = std::max(Delta(j) * etaminus, Deltamin);
+          delta(j) = std::max(Delta(j) * etaminus, deltamin);
           grad(j) = 0;
         }
         params(j) += -boost::math::sign(grad(j)) * Delta(j);
@@ -43,8 +50,6 @@ namespace rprop {
         best_params = params;
       }
     }
-    //  std::cout << "rprop: " << -best << std::endl;
-
     return best_params;
   }
 
