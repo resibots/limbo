7d42078fa69bfeeb7f2d0d8db14f411da17fa8c6
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2015-10-15 10:54:43 +0200

    Small fixes in python tool

--- tools/clean_code.py
+++ tools/clean_code.py
@@ -3,18 +3,6 @@ import fnmatch
 import os
 import sys
 
-# config_data = open('.clang-format', 'r').read()
-# config_data = config_data[4:]
-# config_data = config_data[:len(config_data)-5]
-# config_list = config_data.split('\n')
-
-# config_str = '{'
-# for line in config_list:
-# 	if line[0] != '#':
-# 		config_str = config_str+' '+line+','
-
-# config_str = config_str[:len(config_str)-1] + ' }'
-
 # Format directories and all subfolders using clang-format
 def format_dir(folder, extensions):
 	matches = []
@@ -33,18 +21,21 @@ if __name__ == "__main__":
 	if len(sys.argv) < 1:
 		print "Usage: clean_code.py folder1 [folder2] ..."
 
-	# load config file
+	# config file
 	curr_dir = os.getcwd()
 
 	# change if needed to find the config file
 	if curr_dir[len(curr_dir)-5:] == 'limbo':
 		os.chdir('tools')
+		# copy config file
 		call(["cp", ".clang-format", curr_dir])
 		# change back to old dir
 		os.chdir(curr_dir)
 
+	# format directories specified by the user
 	for arg in sys.argv:
 		format_dir(arg, exts)
 
+	# remove copied file if needed
 	if curr_dir[len(curr_dir)-5:] == 'limbo':
 		call(["rm", ".clang-format"])
