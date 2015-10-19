0688fbaf6c51a5f20144fa3ba54d3cbbc87207bf
Federico Allocati <fede.allocati@gmail.com>
2015-10-19 12:01:29 +0200

    Added macro to define Eigen Vectors

--- src/tests/test_macros.cpp
+++ src/tests/test_macros.cpp
@@ -10,7 +10,8 @@ struct Params {
         BO_PARAM(double, a, 1);
         BO_DYN_PARAM(int, b);
         BO_PARAM_ARRAY(double, c, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-        BO_STRING(d, "d");
+        BO_PARAM_VECTOR(double, d, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+        BO_STRING(e, "e");
     };
 };
 
@@ -37,5 +38,13 @@ BOOST_AUTO_TEST_CASE(test_macros)
     BOOST_CHECK(Params::test::c(4) == 5.0);
     BOOST_CHECK(Params::test::c(5) == 6.0);
 
-    BOOST_CHECK(strcmp(Params::test::d(), "d") == 0);
+    BOOST_CHECK(Params::test::d().size() == 6);
+    BOOST_CHECK(Params::test::d()(0) == 1.0);
+    BOOST_CHECK(Params::test::d()(1) == 2.0);
+    BOOST_CHECK(Params::test::d()(2) == 3.0);
+    BOOST_CHECK(Params::test::d()(3) == 4.0);
+    BOOST_CHECK(Params::test::d()(4) == 5.0);
+    BOOST_CHECK(Params::test::d()(5) == 6.0);
+
+    BOOST_CHECK(strcmp(Params::test::e(), "e") == 0);
 }
