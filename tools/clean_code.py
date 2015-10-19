b5bd78491b5ce235e3261fa6b87211d882896198
Dorian Goepp <dorian.goepp@inria.fr>
2015-10-19 13:09:02 +0200

    Correct the check on number of arguments

--- tools/clean_code.py
+++ tools/clean_code.py
@@ -21,8 +21,9 @@ def format_dir(folder, extensions):
 exts = ['.h', '.c', '.hpp', '.cpp', '.hh', '.cc']
 
 if __name__ == "__main__":
-	if len(sys.argv) < 1:
+	if len(sys.argv) <= 1:
 		print "Usage: clean_code.py folder1 [folder2] ..."
+		sys.exit()
 
 	try:
 		check_output(["clang-format", "-help"])
