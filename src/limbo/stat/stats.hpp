8357ecba3663e747753b0a5ae2524787a7a69822
Federico Allocati <fedeallocati@users.noreply.github.com>
2015-10-28 17:38:31 +0100

    Changed include guard in stat/stats.hpp

--- src/limbo/stat/stats.hpp
+++ src/limbo/stat/stats.hpp
@@ -1,5 +1,5 @@
-#ifndef LIMBO_STATISTICS_STATS_HPP_
-#define LIMBO_STATISTICS_STATS_HPP_
+#ifndef LIMBO_STAT_STATS_HPP
+#define LIMBO_STAT_STATS_HPP
 
 #include <limbo/stat/stat.hpp>
 #include <limbo/stat/acquisitions.hpp>
