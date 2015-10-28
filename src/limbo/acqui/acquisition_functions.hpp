7c9ca76fc8cc7bc3e4affc4c73ca954d13eb5208
Federico Allocati <fedeallocati@users.noreply.github.com>
2015-10-28 16:48:17 +0100

    Changed include guard in acquisition_functions.hpp

--- src/limbo/acqui/acquisition_functions.hpp
+++ src/limbo/acqui/acquisition_functions.hpp
@@ -1,5 +1,5 @@
-#ifndef ACQUISITION_FUNCTIONS_HPP_
-#define ACQUISITION_FUNCTIONS_HPP_
+#ifndef LIMBO_ACQUI_ACQUISITION_FUNCTIONS_HPP
+#define LIMBO_ACQUI_ACQUISITION_FUNCTIONS_HPP
 
 #include <limbo/acqui/ucb.hpp>
 #include <limbo/acqui/gp_ucb.hpp>
