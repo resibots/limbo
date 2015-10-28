6c4829cd0bce36ced95fb5c9222ef5922bef5e62
Federico Allocati <fedeallocati@users.noreply.github.com>
2015-10-28 17:28:46 +0100

    Changed include guard in mean/mean_functions.hpp

--- src/limbo/mean/mean_functions.hpp
+++ src/limbo/mean/mean_functions.hpp
@@ -1,5 +1,5 @@
-#ifndef MEAN_FUNCTIONS_HPP_
-#define MEAN_FUNCTIONS_HPP_
+#ifndef LIMBO_MEAN_MEAN_FUNCTIONS_HPP
+#define LIMBO_MEAN_MEAN_FUNCTIONS_HPP
 
 #include <limbo/mean/null_function.hpp>
 #include <limbo/mean/constant.hpp>
