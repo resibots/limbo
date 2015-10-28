a1d5fae0e2a93a66c93e122e07a744b1e7587108
Federico Allocati <fedeallocati@users.noreply.github.com>
2015-10-28 17:39:31 +0100

    Changed include guard in stop/stopping_criteria.hpp

--- src/limbo/stop/stopping_criteria.hpp
+++ src/limbo/stop/stopping_criteria.hpp
@@ -1,5 +1,5 @@
-#ifndef STOPPING_CRITERIA_HPP_
-#define STOPPING_CRITERIA_HPP_
+#ifndef LIMBO_STOP_STOPPING_CRITERIA_HPP
+#define LIMBO_STOP_STOPPING_CRITERIA_HPP
 
 #include <limbo/stop/max_iterations.hpp>
 #include <limbo/stop/max_predicted_value.hpp>
