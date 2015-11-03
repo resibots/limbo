c8ca8289f551ec560092cc9c1823b442f8af3684
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2015-11-03 11:36:30 +0100

    Added missing header in opt.hpp

--- src/limbo/opt.hpp
+++ src/limbo/opt.hpp
@@ -3,6 +3,7 @@
 
 #include <limbo/opt/rprop.hpp>
 #include <limbo/opt/cmaes.hpp>
+#include <limbo/opt/exhaustive_search.hpp>
 #include <limbo/opt/impl/inner_struct.hpp>
 #include <limbo/opt/impl/inner_cmaes.hpp>
 #include <limbo/opt/impl/inner_exhaustive_search.hpp>
