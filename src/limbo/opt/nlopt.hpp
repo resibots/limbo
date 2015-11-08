6766560cf4453f7f41def696e0cbf23506b4f168
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2015-11-08 22:15:07 +0100

    Ran code formatter

--- src/limbo/opt/nlopt.hpp
+++ src/limbo/opt/nlopt.hpp
@@ -15,43 +15,41 @@ namespace limbo {
             template <typename F>
             Eigen::VectorXd operator()(const F& f) const
             {
-              nlopt::opt opt(Algorithm, f.param_size());
+                nlopt::opt opt(Algorithm, f.param_size());
 
-              opt.set_max_objective(this->nlopt_func<F>, (void*)&f);
+                opt.set_max_objective(this->nlopt_func<F>, (void*)&f);
 
-              std::vector<double> x;
-              x.resize(f.init().size());
-              Eigen::VectorXd::Map(&x[0], f.init().size()) = f.init();
+                std::vector<double> x;
+                x.resize(f.init().size());
+                Eigen::VectorXd::Map(&x[0], f.init().size()) = f.init();
 
-              opt.set_ftol_rel(1e-8);
+                opt.set_ftol_rel(1e-8);
 
-              double min;
+                double min;
 
-              opt.optimize(x, min);
+                opt.optimize(x, min);
 
-              return Eigen::VectorXd::Map(x.data(), x.size());
+                return Eigen::VectorXd::Map(x.data(), x.size());
             }
 
-          protected:
-            template<typename F>
-            static double nlopt_func(const std::vector<double> &x, std::vector<double> &grad, void *my_func_data)
+        protected:
+            template <typename F>
+            static double nlopt_func(const std::vector<double>& x, std::vector<double>& grad, void* my_func_data)
             {
-              F* f = (F*)(my_func_data);
-              Eigen::VectorXd params = Eigen::VectorXd::Map(x.data(), x.size());
-              double v;
-              if(!grad.empty())
-              {
-                Eigen::VectorXd g;
-                auto p = f->utility_and_grad(params);
-                v = std::get<0>(p);
-                g = std::get<1>(p);
-                Eigen::VectorXd::Map(&grad[0], g.size()) = g;
-              }
-              else
-              {
-                v = f->utility(params);
-              }
-              return v;
+                F* f = (F*)(my_func_data);
+                Eigen::VectorXd params = Eigen::VectorXd::Map(x.data(), x.size());
+                double v;
+                if (!grad.empty()) {
+                    Eigen::VectorXd g;
+                    auto p = f->utility_and_grad(params);
+                    v = std::get<0>(p);
+                    g = std::get<1>(p);
+                    Eigen::VectorXd::Map(&grad[0], g.size()) = g;
+                }
+                else {
+                    v = f->utility(params);
+                }
+                return v;
             }
         };
     }
--- src/tests/test_nlopt.cpp
+++ src/tests/test_nlopt.cpp
@@ -7,7 +7,6 @@
 #include <limbo/opt/nlopt.hpp>
 
 struct Params {
-
 };
 
 struct TestOpt {
@@ -16,16 +15,16 @@ public:
 
     double utility(const Eigen::VectorXd& params) const
     {
-        return params(0)*params(0)+params(1)*params(1);
+        return params(0) * params(0) + params(1) * params(1);
     }
 
     std::pair<double, Eigen::VectorXd> utility_and_grad(const Eigen::VectorXd& params) const
     {
-        double v = -params(0)*params(0)-params(1)*params(1);
+        double v = -params(0) * params(0) - params(1) * params(1);
         Eigen::VectorXd grad(2);
-        grad(0) = -2*params(0);
-        grad(1) = -2*params(1);
-        return std::make_pair(v,grad);
+        grad(0) = -2 * params(0);
+        grad(1) = -2 * params(1);
+        return std::make_pair(v, grad);
     }
 
     size_t param_size() const
@@ -35,15 +34,15 @@ public:
 
     Eigen::VectorXd init() const
     {
-        return (Eigen::VectorXd::Random(param_size()).array() + 1)/2.0;
+        return (Eigen::VectorXd::Random(param_size()).array() + 1) / 2.0;
     }
 };
 
 BOOST_AUTO_TEST_CASE(test_nlopt_simple)
 {
-  TestOpt util;
-  Eigen::VectorXd g = limbo::opt::NLOpt<Params>()(util);
+    TestOpt util;
+    Eigen::VectorXd g = limbo::opt::NLOpt<Params>()(util);
 
-  BOOST_CHECK_SMALL(g(0), 0.00000001);
-  BOOST_CHECK_SMALL(g(1), 0.00000001);
+    BOOST_CHECK_SMALL(g(0), 0.00000001);
+    BOOST_CHECK_SMALL(g(1), 0.00000001);
 }
