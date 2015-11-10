1426339d9102edcc172aa0d07d10803e60a56382
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2015-11-10 10:27:32 +0100

    No need to print something in random sampling init

--- src/limbo/init/random_sampling.hpp
+++ src/limbo/init/random_sampling.hpp
@@ -1,8 +1,6 @@
 #ifndef LIMBO_INIT_RANDOM_SAMPLING_HPP
 #define LIMBO_INIT_RANDOM_SAMPLING_HPP
 
-#include <iostream>
-
 #include <Eigen/Core>
 
 #include <limbo/tools/rand.hpp>
@@ -20,7 +18,6 @@ namespace limbo {
                     Eigen::VectorXd new_sample(F::dim_in);
                     for (int i = 0; i < F::dim_in; i++)
                         new_sample[i] = tools::rand<double>(0, 1);
-                    std::cout << "random sample:" << new_sample.transpose() << std::endl;
                     opt.add_new_sample(new_sample, feval(new_sample));
                 }
             }
