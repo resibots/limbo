c206abd5554e090b5566e165f8d475cad39cfdef
Jean-Baptiste Mouret <mouret@isir.upmc.fr>
2015-11-17 14:40:27 +0100

    compilation with osx

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,7 @@ html_theme = 'alabaster'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['/Library/Python/2.7/site-packages']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
