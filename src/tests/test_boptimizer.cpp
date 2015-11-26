4b0dca6106306dae708edf93ad3429353af1c989
Federico Allocati <fede.allocati@gmail.com>
2015-11-26 13:33:18 +0100

    Added Observations stat

--- src/tests/test_boptimizer.cpp
+++ src/tests/test_boptimizer.cpp
@@ -87,7 +87,7 @@ BOOST_AUTO_TEST_CASE(test_bo_gp)
     typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
     // typedef mean_functions::MeanFunctionARD<Params, mean_functions::MeanData<Params>> Mean_t;
     typedef mean::Data<Params> Mean_t;
-    typedef boost::fusion::vector<stat::Samples<Params>> Stat_t;
+    typedef boost::fusion::vector<stat::Samples<Params>, stat::Observations<Params>> Stat_t;
     typedef init::NoInit<Params> Init_t;
     typedef model::GP<Params, Kernel_t, Mean_t> GP_t;
     typedef acqui::UCB<Params, GP_t> Acqui_t;
@@ -108,7 +108,7 @@ BOOST_AUTO_TEST_CASE(test_bo_gp_auto)
     typedef opt::GridSearch<Params> AcquiOpt_t;
     typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
     typedef mean::Data<Params> Mean_t;
-    typedef boost::fusion::vector<stat::Samples<Params>> Stat_t;
+    typedef boost::fusion::vector<stat::Samples<Params>, stat::Observations<Params>> Stat_t;
     typedef init::RandomSampling<Params> Init_t;
     typedef model::GP<Params, Kernel_t, Mean_t, model::gp::KernelLFOpt<Params>> GP_t;
     typedef acqui::UCB<Params, GP_t> Acqui_t;
@@ -127,7 +127,7 @@ BOOST_AUTO_TEST_CASE(test_bo_gp_auto_mean)
     typedef opt::GridSearch<Params> AcquiOpt_t;
     typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
     typedef mean::FunctionARD<Params, mean::Data<Params>> Mean_t;
-    typedef boost::fusion::vector<stat::Samples<Params>> Stat_t;
+    typedef boost::fusion::vector<stat::Samples<Params>, stat::Observations<Params>> Stat_t;
     typedef init::RandomSampling<Params> Init_t;
     typedef model::GP<Params, Kernel_t, Mean_t, model::gp::KernelMeanLFOpt<Params>> GP_t;
     typedef acqui::UCB<Params, GP_t> Acqui_t;
@@ -146,7 +146,7 @@ BOOST_AUTO_TEST_CASE(test_bo_gp_mean)
     typedef opt::GridSearch<Params> AcquiOpt_t;
     typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
     typedef mean::FunctionARD<Params, mean::Data<Params>> Mean_t;
-    typedef boost::fusion::vector<stat::Samples<Params>> Stat_t;
+    typedef boost::fusion::vector<stat::Samples<Params>, stat::Observations<Params>> Stat_t;
     typedef init::RandomSampling<Params> Init_t;
     typedef model::GP<Params, Kernel_t, Mean_t, model::gp::MeanLFOpt<Params>> GP_t;
     typedef acqui::UCB<Params, GP_t> Acqui_t;
