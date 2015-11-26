cefb05e2e94de347b384631b8e6b3d7340bb79b7
Federico Allocati <fede.allocati@gmail.com>
2015-11-26 14:22:02 +0100

    Added Blacklisted Samples Stat

--- src/limbo/stat.hpp
+++ src/limbo/stat.hpp
@@ -4,6 +4,7 @@
 #include <limbo/stat/best_aggregated_observations.hpp>
 #include <limbo/stat/best_observations.hpp>
 #include <limbo/stat/best_samples.hpp>
+#include <limbo/stat/bl_samples.hpp>
 #include <limbo/stat/aggregated_observations.hpp>
 #include <limbo/stat/observations.hpp>
 #include <limbo/stat/samples.hpp>
