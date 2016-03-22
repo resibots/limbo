94f79319dffaf3c584340e8c8db6094e94c5a0e9
Federico Allocati <fede.allocati@gmail.com>
2016-03-21 23:47:14 -0300

    Implementation and test of Chained Optimizer

--- src/tests/test_optimizers.cpp
+++ src/tests/test_optimizers.cpp
@@ -7,6 +7,7 @@
 #include <limbo/opt/cmaes.hpp>
 #include <limbo/opt/grid_search.hpp>
 #include <limbo/opt/random_point.hpp>
+#include <limbo/opt/chained.hpp>
 
 using namespace limbo;
 
@@ -103,4 +104,23 @@ BOOST_AUTO_TEST_CASE(test_grid_search_bi_dim)
     BOOST_CHECK_SMALL(best_point(1), 0.000001);
     // TO-DO: Maybe alter a little grid search so not to call more times the utility function
     BOOST_CHECK_EQUAL(bidim_calls, (Params::opt_gridsearch::bins() + 1) * (Params::opt_gridsearch::bins() + 1) + 21);
+}
+
+BOOST_AUTO_TEST_CASE(test_chained)
+{
+    using namespace limbo;
+
+    typedef opt::GridSearch<Params> opt_1_t;
+    typedef opt::RandomPoint<Params> opt_2_t;
+    typedef opt::GridSearch<Params> opt_3_t;
+    typedef opt::GridSearch<Params> opt_4_t;
+    opt::Chained<Params, opt_1_t, opt_2_t, opt_3_t, opt_4_t> optimizer;
+
+    monodim_calls = 0;
+    Eigen::VectorXd best_point = optimizer(acqui_mono, Eigen::VectorXd::Constant(1, 0.5), true);
+
+    BOOST_CHECK_EQUAL(best_point.size(), 1);
+    BOOST_CHECK(best_point(0) > 0 || std::abs(best_point(0)) < 1e-7);
+    BOOST_CHECK(best_point(0) < 1 || std::abs(best_point(0) - 1) < 1e-7);
+    BOOST_CHECK_EQUAL(monodim_calls, (Params::opt_gridsearch::bins() + 1) * 3);
 }
