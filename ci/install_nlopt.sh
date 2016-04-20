874401c2299d7421c6f12ebe2593701dcf509f63
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2016-04-20 12:47:59 +0200

    Minor fix in ci shs

--- ci/install_nlopt.sh
+++ ci/install_nlopt.sh
@@ -1,3 +1,5 @@
+sudo apt-get -qq update
+sudo apt-get -qq --yes --force-yes install autoconf automake
 cd && wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
 tar -zxvf nlopt-2.4.2.tar.gz && cd nlopt-2.4.2
 ./configure -with-cxx --enable-shared --without-python --without-matlab --without-octave
