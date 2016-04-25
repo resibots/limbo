d0e2b9b6c31590df32be949746da1e7e5e0c005d
Federico Allocati <fedeallocati@users.noreply.github.com>
2016-04-25 10:07:58 -0300

    Updated inner optimization to match BayesOpt

--- src/benchmarks/limbo/simple.cpp
+++ src/benchmarks/limbo/simple.cpp
@@ -11,80 +11,60 @@ struct Params {
   struct bayes_opt_bobase {
     BO_PARAM(bool, stats_enabled, false);
   };
-    struct bayes_opt_boptimizer {
-        BO_PARAM(double, noise, 1e-10);
-        BO_PARAM(int, dump_period, -1);
-    };
-    struct stop_maxiterations {
-        BO_PARAM(int, iterations, 190);
-    };
-
-    struct kernel_maternfivehalfs {
-        BO_PARAM(double, sigma, 1);
-        BO_PARAM(double, l, 1);
-    };
-    struct acqui_ucb {
-        BO_PARAM(double, alpha, 0.125);
-    };
-    struct init_randomsampling {
-        BO_PARAM(int, samples, 10);
-    };
-    struct opt_nloptnograd {
-        BO_DYN_PARAM(int, iterations);
-        BO_PARAM(double, epsilon, 1e-8);
-    };
-
+  struct bayes_opt_boptimizer {
+      BO_PARAM(double, noise, 1e-10);
+      BO_PARAM(int, dump_period, -1);
+  };
+  struct stop_maxiterations {
+      BO_PARAM(int, iterations, 190);
+  };
+  struct kernel_maternfivehalfs {
+      BO_PARAM(double, sigma, 1);
+      BO_PARAM(double, l, 1);
+  };
+  struct acqui_ucb {
+      BO_PARAM(double, alpha, 0.125);
+  };
+  struct init_randomsampling {
+      BO_PARAM(int, samples, 10);
+  };
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+      BO_PARAM(double, epsilon, 1e-12);
+  };
+  struct opt_nloptgrad {
+      BO_DYN_PARAM(int, iterations);
+      BO_PARAM(double, epsilon, 1e-12);
+  };
   struct mean_constant {
     BO_PARAM(double, constant, 1);
   };
 };
 
-BO_DECLARE_DYN_PARAM(int, Params::opt_nloptnograd, iterations);
-
-template <typename Params>
-struct StaticInit {
-    template <typename StateFunction, typename AggregatorFunction, typename Opt>
-    void operator()(const StateFunction& seval, const AggregatorFunction&, Opt& opt) const
-    {
-        Eigen::Vector2d x;
-        x(0) = -0.055497063038156969690135505676;
-        x(1) = -0.351795677434771201331992637257;
-        opt.add_new_sample(x, seval(x));
-        x(0) = 0.240911582092300627645879219810;
-        x(1) = -0.351795677434771201331992637257;
-        opt.add_new_sample(x, seval(x));
-        x(0) = -0.351795677434771201331992637257;
-        x(1) = -0.343720034134076580133895794673;
-        opt.add_new_sample(x, seval(x));
-        x(0) = 0.055387032304313603995977911771;
-        x(1) = -0.076908787408790806346486820340;
-        opt.add_new_sample(x, seval(x));
-        x(0) = 0.351795677434771201331992637257;
-        x(1) = -0.076908787408790806346486820339;
-        opt.add_new_sample(x, seval(x));
-        x(0) = -0.351795677434771201331992637257;
-        x(1) = -0.047311389003618982797881069187;
-        opt.add_new_sample(x, seval(x));
-        x(0) = -0.132623434010086730621834242216;
-        x(1) = 0.152242144215576109267055784036;
-        opt.add_new_sample(x, seval(x));
-        x(0) = 0.351795677434771201331992637257;
-        x(1) = 0.219499857721666790989527905147;
-        opt.add_new_sample(x, seval(x));
-        x(0) = -0.351795677434771201331992637257;
-        x(1) = 0.351795677434771201331992637257;
-        opt.add_new_sample(x, seval(x));
-        x(0) = 0.086548809414597740088324152827;
-        x(1) = 0.351795677434771201331992637257;
-        opt.add_new_sample(x, seval(x));
-    }
+struct DirectParams {
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+      BO_PARAM(double, epsilon, 1e-12);
+  };
 };
+
+struct BobyqaParams {
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+      BO_PARAM(double, epsilon, 1e-12);
+  };
+};
+
+BO_DECLARE_DYN_PARAM(int, DirectParams::opt_nloptnograd, iterations);
+BO_DECLARE_DYN_PARAM(int, BobyqaParams::opt_nloptnograd, iterations);
 
 template <typename Optimizer, typename Function>
 void benchmark(const std::string& name)
 {
     int iters_base = 250;
-    Params::opt_nloptnograd::set_iterations(iters_base * Function::dim_in);
+    DirectParams::opt_nloptnograd::set_iterations(static_cast<int>(iters_base * Function::dim_in * 0.9));
+    BobyqaParams::opt_nloptnograd::set_iterations(iters_base - Params::opt_nloptnograd::iterations());
+
     auto t1 = std::chrono::steady_clock::now();
     Optimizer opt;
     Benchmark<Function> target;
@@ -112,6 +92,7 @@ int main()
 
     typedef kernel::MaternFiveHalfs<Params> Kernel_t;
     typedef opt::NLOptNoGrad<Params, nlopt::GN_DIRECT_L> AcquiOpt_t;
+    typedef opt::Chained<Params, opt::NLOptNoGrad<DirectParams, nlopt::GN_DIRECT_L>, opt::NLOptNoGrad<BobyqaParams, nlopt::LN_BOBYQA>> AcquiOpt_t;
     typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
     // typedef mean_functions::MeanFunctionARD<Params, mean_functions::MeanData<Params>> Mean_t;
     typedef mean::Constant<Params> Mean_t;
