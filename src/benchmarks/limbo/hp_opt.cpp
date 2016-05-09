b619e630cc2cec1ef516aca04c7d1b1ead2efdaa
Antoine Cully <a.cully@imperial.ac.uk>
2016-05-09 14:05:38 +0100

    add benchmark with HP opt

---
+++ src/benchmarks/limbo/hp_opt.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <chrono>
+
+#include <limbo/limbo.hpp>
+
+#include "testfunctions.hpp"
+
+using namespace limbo;
+
+struct Params {
+  struct bayes_opt_bobase {
+    BO_PARAM(bool, stats_enabled, false);
+  };
+  struct bayes_opt_boptimizer {
+      BO_PARAM(double, noise, 1e-10);
+      BO_PARAM(int, dump_period, -1);
+  };
+  struct stop_maxiterations {
+      BO_PARAM(int, iterations, 190);
+  };
+  struct kernel_maternfivehalfs {
+      BO_PARAM(double, sigma, 1);
+      BO_PARAM(double, l, 1);
+  };
+  struct acqui_ucb {
+      BO_PARAM(double, alpha, 0.125);
+  };
+  struct init_randomsampling {
+      BO_PARAM(int, samples, 10);
+  };
+  struct mean_constant {
+    BO_PARAM(double, constant, 1);
+  };
+  struct opt_rprop : public defaults::opt_rprop {
+  };
+
+};
+
+struct DirectParams {
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+  };
+};
+
+struct BobyqaParams {
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+  };
+};
+struct BobyqaParams_HP {
+  struct opt_nloptnograd {
+      BO_DYN_PARAM(int, iterations);
+  };
+};
+
+BO_DECLARE_DYN_PARAM(int, DirectParams::opt_nloptnograd, iterations);
+BO_DECLARE_DYN_PARAM(int, BobyqaParams::opt_nloptnograd, iterations);
+BO_DECLARE_DYN_PARAM(int, BobyqaParams_HP::opt_nloptnograd, iterations);
+
+template <typename Optimizer, typename Function>
+void benchmark(const std::string& name)
+{
+    int iters_base = 250;
+    DirectParams::opt_nloptnograd::set_iterations(static_cast<int>(iters_base * Function::dim_in * 0.9));
+    BobyqaParams::opt_nloptnograd::set_iterations(iters_base * Function::dim_in - DirectParams::opt_nloptnograd::iterations());
+
+    BobyqaParams_HP::opt_nloptnograd::set_iterations(10 * Function::dim_in);
+
+    auto t1 = std::chrono::steady_clock::now();
+    Optimizer opt;
+    Benchmark<Function> target;
+    opt.optimize(target);
+    auto time_running = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t1).count();
+    std::cout.precision(17);
+    std::cout << std::endl;
+    auto best = opt.best_observation();
+    double accuracy = target.accuracy(best);
+
+    std::cout << name << std::endl;
+    std::cout << "Result: " << std::fixed << opt.best_sample().transpose() << " -> " << best << std::endl;
+    std::cout << "Smallest difference: " << accuracy << std::endl;
+    std::cout << "Time running: " << time_running << "ms" << std::endl
+              << std::endl;
+
+    std::ofstream res_file(name + ".dat",std::ios_base::out | std::ios_base::app);
+    res_file.precision(17);
+    res_file << std::fixed << accuracy << " " << time_running << std::endl;
+}
+
+int main()
+{
+  srand(time(NULL));
+
+    typedef kernel::SquaredExpARD<Params> Kernel_t;
+    typedef opt::Chained<Params, opt::NLOptNoGrad<DirectParams, nlopt::GN_DIRECT_L>, opt::NLOptNoGrad<BobyqaParams, nlopt::LN_BOBYQA>> AcquiOpt_t;
+    typedef boost::fusion::vector<stop::MaxIterations<Params>> Stop_t;
+    // typedef mean_functions::MeanFunctionARD<Params, mean_functions::MeanData<Params>> Mean_t;
+    typedef mean::Constant<Params> Mean_t;
+    typedef boost::fusion::vector<> Stat_t;
+    typedef init::RandomSampling<Params> Init_t;
+    typedef model::GP<Params, Kernel_t, Mean_t,model::gp::KernelLFOpt<Params,opt::NLOptNoGrad<BobyqaParams_HP, nlopt::LN_BOBYQA> > > GP_t;
+    typedef acqui::UCB<Params, GP_t> Acqui_t;
+
+    typedef bayes_opt::BOptimizer<Params, modelfun<GP_t>, initfun<Init_t>, acquifun<Acqui_t>, acquiopt<AcquiOpt_t>, statsfun<Stat_t>, stopcrit<Stop_t>> Opt_t;
+
+    benchmark<Opt_t, BraninNormalized>("branin");
+    benchmark<Opt_t, Hartman6>("hartman6");
+    benchmark<Opt_t, Hartman3>("hartman3");
+    benchmark<Opt_t, Rastrigin>("rastrigin");
+    benchmark<Opt_t, Sphere>("sphere");
+    benchmark<Opt_t, Ellipsoid>("ellipsoid");
+    benchmark<Opt_t, GoldenPrice>("goldenprice");
+    benchmark<Opt_t, SixHumpCamel>("sixhumpcamel");
+
+    return 0;
+}
