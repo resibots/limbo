26bf2cf22eeaaa57db2112c1919c90be294b6f06
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2016-09-14 11:25:56 +0200

    Small fix in EI

--- src/limbo/acqui/ei.hpp
+++ src/limbo/acqui/ei.hpp
@@ -101,7 +101,7 @@ namespace limbo {
                 double f_max = *std::max_element(rewards.begin(), rewards.end());
                 // Calculate Z and \Phi(Z) and \phi(Z)
                 double Z = (afun(mu) - f_max - Params::acqui_ei::jitter()) / sigma;
-                double phi = std::exp(-0.5 * std::pow(Z, 2.0)) / std::sqrt(2.0 * M_PI);
+                double phi = std::exp(-0.5 * std::pow(Z, 2.0)) / std::sqrt(2.0 * sigma_sq * M_PI);
                 double Phi = 0.5 * (1.0 + std::erf(Z / std::sqrt(2)));
 
                 return Z * Phi + sigma * phi;
