eea71cc2e6f522f12bc19eeca7122717b4b55efc
Jean-Baptiste Mouret <mouret@isir.upmc.fr>
2016-09-15 19:34:04 +0200

    fix ./waf --create when the experiment is already here

--- waf_tools/limbo.py
+++ waf_tools/limbo.py
@@ -4,32 +4,32 @@
 #| This project has received funding from the European Research Council (ERC) under
 #| the European Union's Horizon 2020 research and innovation programme (grant
 #| agreement No 637972) - see http://www.resibots.eu
-#| 
+#|
 #| Contributor(s):
 #|   - Jean-Baptiste Mouret (jean-baptiste.mouret@inria.fr)
 #|   - Antoine Cully (antoinecully@gmail.com)
 #|   - Kontantinos Chatzilygeroudis (konstantinos.chatzilygeroudis@inria.fr)
 #|   - Federico Allocati (fede.allocati@gmail.com)
 #|   - Vaios Papaspyros (b.papaspyros@gmail.com)
-#| 
+#|
 #| This software is a computer library whose purpose is to optimize continuous,
 #| black-box functions. It mainly implements Gaussian processes and Bayesian
 #| optimization.
 #| Main repository: http://github.com/resibots/limbo
 #| Documentation: http://www.resibots.eu/limbo
-#| 
+#|
 #| This software is governed by the CeCILL-C license under French law and
 #| abiding by the rules of distribution of free software.  You can  use,
 #| modify and/ or redistribute the software under the terms of the CeCILL-C
 #| license as circulated by CEA, CNRS and INRIA at the following URL
 #| "http://www.cecill.info".
-#| 
+#|
 #| As a counterpart to the access to the source code and  rights to copy,
 #| modify and redistribute granted by the license, users are provided only
 #| with a limited warranty  and the software's author,  the holder of the
 #| economic rights,  and the successive licensors  have only  limited
 #| liability.
-#| 
+#|
 #| In this respect, the user's attention is drawn to the risks associated
 #| with loading,  using,  modifying and/or developing or reproducing the
 #| software by the user in light of its specific status of free software,
@@ -40,10 +40,10 @@
 #| requirements in conditions enabling the security of their systems and/or
 #| data to be ensured and,  more generally, to use and operate it in the
 #| same conditions as regards security.
-#| 
+#|
 #| The fact that you are presently reading this means that you have had
 #| knowledge of the CeCILL-C license and that you accept its terms.
-#| 
+#|
 import os
 import stat
 import subprocess
@@ -97,6 +97,9 @@ def create_variants(bld, source, uselib_local,
 def create_exp(name, opt):
     if not os.path.exists('exp'):
         os.makedirs('exp')
+    if os.path.exists('exp/' + name):
+        print 'ERROR: experiment ' + name + ' already exists. Please remove it if you want to re-create it from scratch.'
+        return
     os.mkdir('exp/' + name)
 
     ws_tpl = ""
