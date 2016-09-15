6610f7b062522ca6c0614335d35286c78b9ff6ce
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2016-09-15 15:24:12 +0200

    Fix for NLOpt CI

--- ci/install_nlopt.sh
+++ ci/install_nlopt.sh
@@ -1,6 +1,6 @@
 sudo apt-get -qq update
 sudo apt-get -qq --yes --force-yes install autoconf automake
-cd && wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
+cd && wget http://members.loria.fr/JBMouret/mirrors/nlopt-2.4.2.tar.gz
 tar -zxvf nlopt-2.4.2.tar.gz && cd nlopt-2.4.2
 ./configure -with-cxx --enable-shared --without-python --without-matlab --without-octave
 sudo make install
