c7c6b415267909538054be9fa508a582e74063ea
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2017-06-30 23:00:05 +0200

    do not know why this has been changed...

--- src/limbo/bayes_opt/bo_base.hpp
+++ src/limbo/bayes_opt/bo_base.hpp
@@ -182,7 +182,9 @@ namespace limbo {
             // defaults
             struct defaults {
                 using init_t = init::RandomSampling<Params>; // 1
-                using model_t = model::GPBasic<Params>; // 2
+                using kf_t = kernel::Exp<Params>;
+                using mean_t = mean::Data<Params>;
+                using model_t = model::GP<Params, kf_t, mean_t>; // 2
                 // WARNING: you have to specify the acquisition  function
                 // if you use a custom model
                 using acqui_t = acqui::UCB<Params, model_t>; // 3
