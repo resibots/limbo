c24536604a00ae440d2c083a6e47515ff98f4a9e
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2017-08-30 16:58:22 +0200

    insert benchmark in pre_script

--- docs/pre_script.sh
+++ docs/pre_script.sh
@@ -8,3 +8,9 @@ cd ..
 ./waf default_params
 
 
+echo "getting the latest benchmark result (needs to be in $HOME/limbo_benchmarks")
+# get the last benchmark
+DIR=$HOME/limbo_benchmarks
+BENCHMARKS=$DIR/`ls $DIR|sort -n|head -n 1`
+cp $BENCHMARKS/bo_benchmarks.rst docs/benchmarks.rst
+cp -r $BENCHMARKS/fig_benchmarks docs
