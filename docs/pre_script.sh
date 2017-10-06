d81a246ed49ba20a1f66f210673b5eb1204d4cb8
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2017-10-06 22:46:13 +0200

    [ci skip] sort by date

--- docs/pre_script.sh
+++ docs/pre_script.sh
@@ -11,13 +11,13 @@ cd ..
 echo "getting the latest benchmark result (needs to be in $HOME/limbo_benchmarks)"
 # get the last benchmark
 DIR=$HOME/limbo_benchmarks
-BENCHMARKS=$DIR/`ls $DIR|sort -n|head -n 1`
+BENCHMARKS=$DIR/`ls -t $DIR | head -n 1`
 cp $BENCHMARKS/bo_benchmarks.rst docs/bo_benchmarks.rst
 cp -r $BENCHMARKS/fig_benchmarks docs
 
 echo "getting the latest regression benchmark result (needs to be in $HOME/limbo_reg_benchmarks)"
 DIR=$HOME/limbo_reg_benchmarks
-BENCHMARKS=$DIR/`ls $DIR|sort -n|head -n 1`
+BENCHMARKS=$DIR/`ls -t $DIR | |head -n 1`
 cp $BENCHMARKS/regression_benchmarks.rst docs/reg_benchmarks.rst
 cp -r $BENCHMARKS/regression_benchmarks_figs docs/fig_benchmarks
 
