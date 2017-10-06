1d6bce70d9228d9366e3f4621a500ef280d62530
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2017-10-06 22:10:32 +0200

    [ci skip] bug in the pre-script (docs)

--- docs/pre_script.sh
+++ docs/pre_script.sh
@@ -16,7 +16,7 @@ cp $BENCHMARKS/bo_benchmarks.rst docs/bo_benchmarks.rst
 cp -r $BENCHMARKS/fig_benchmarks docs
 
 echo "getting the latest regression benchmark result (needs to be in $HOME/limbo_reg_benchmarks)"
-DIR=$HOME/limbo_ref_benchmarks
+DIR=$HOME/limbo_reg_benchmarks
 BENCHMARKS=$DIR/`ls $DIR|sort -n|head -n 1`
 cp $BENCHMARKS/regression_benchmarks.rst docs/reg_benchmarks.rst
 cp -r $BENCHMARKS/regression_benchmarks_figs docs
