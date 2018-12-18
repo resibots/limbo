999e71b5caaf1781e530394a5128117c952af356
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2018-12-18 09:28:22 +0100

    Fix travis Eigen3.3

--- ci/install_eigen3_3.sh
+++ ci/install_eigen3_3.sh
@@ -1,6 +1,6 @@
 sudo apt-get -qq update
 sudo apt-get -qq --yes --force-yes install libblas-dev liblapacke liblapacke-dev
-cd && hg clone https://bitbucket.org/eigen/eigen
+cd && hg clone --insecure https://bitbucket.org/eigen/eigen
 cd eigen
 hg up 3.3
 mkdir build && cd build
